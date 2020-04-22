da75f40f18a82cb678937cb63876e451486dd104
Abdullah Celen <abdullahcelen009@gmail.com>
2020-04-22 21:25:25 +0100

    add search div

--- script.js
+++ script.js
@@ -6,54 +6,41 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  // rootElem.innerHTML = `${episodeList.length} episode(s)`;
+  //adds search box
+  let searchDiv = document.createElement("div");
+  searchDiv.className = "myInput";
+  rootElem.appendChild(searchDiv);
+  let searchLabel = document.createElement("label");
+  let searchBox = document.createElement("input");
+  searchBox.setAttribute("type", "text");
+  searchDiv.appendChild(searchBox);
+  searchDiv.appendChild(searchLabel);
+  searchLabel.innerHTML = `<p class = "label">    Displaying ${episodeList.length}/${episodeList.length} episode(s)</p>`;
 
+  episodeList.forEach((episodeList) => {
+    let container = document.createElement("div");
+    container.className = "column";
+    rootElem.appendChild(container);
+    let name = episodeList.name;
+    let number = ` - S${
+      episodeList.season < 10 ? "0" + episodeList.season : episodeList.season
+    }E${
+      episodeList.number < 10 ? "0" + episodeList.number : episodeList.number
+    }`;
+    let image = episodeList.image.medium;
+    let summary = episodeList.summary;
 
-episodeList.forEach((episodeList) =>{
-let container = document.createElement("div");
-container.className = "column";
-rootElem.appendChild(container);
-let name = episodeList.name;
-let number = ` - S${episodeList.season < 10 ? "0" + episodeList.season : episodeList.season }E${episodeList.number < 10 ? "0" + episodeList.number : episodeList.number }`;
-let image = episodeList.image.medium;
-let summary = episodeList.summary;
+    let h1El = document.createElement("h1");
+    container.appendChild(h1El);
+    h1El.textContent = name + number;
 
-let h1El = document.createElement("h1");
-container.appendChild(h1El);
-h1El.textContent = name + number;
+    let elementImg = document.createElement("img");
+    container.appendChild(elementImg);
+    elementImg.src = image;
 
-
-let elementImg = document.createElement("img");
-container.appendChild(elementImg);
-elementImg.src = image;
-
-let elementSummary = document.createElement("p");
-container.appendChild(elementSummary)
-elementSummary.innerHTML = summary;
-
-
-});
-
+    let elementSummary = document.createElement("p");
+    container.appendChild(elementSummary);
+    elementSummary.innerHTML = summary;
+  });
 }
 window.onload = setup;
-
-function myFunction(episodeList) {
-  var input, filter, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
- 
-  for (i = 0; i < episodeList.length; i++) {
-      a = episodeList[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          episodeList[i].style.display = "";
-      } else {
-          episodeList[i].style.display = "none";
-      }
-  }
-}
-
-
-
-
-
